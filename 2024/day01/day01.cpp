7f005385a338ecbb94c973bb3fdc9c489e2ca06b
Gijsdeman <github@deman.dev>
2024-12-02 12:35:20 +0100

    feat(2024-01): add day 1 solution

---
+++ 2024/day01/day01.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <unordered_set>
+
+#include "day01.h"
+
+int64_t Day01::part1(std::ifstream& file) {
+  std::vector<int32_t> left;
+  std::vector<int32_t> right;
+  std::string line;
+
+  while (std::getline(file, line)) {
+    std::istringstream lineStream(line);
+    if (int numRight, numLeft; lineStream >> numLeft >> numRight) {
+      left.push_back(numLeft);
+      right.push_back(numRight);
+    }
+  }
+
+  std::ranges::sort(left);
+  std::ranges::sort(right);
+
+  int result = 0;
+  for (int i = 0; i < left.size(); i++) {
+    result += abs(left[i] - right[i]);
+  }
+
+  return result;
+}
+
+
+int64_t Day01::part2(std::ifstream& file) {
+  std::vector<int32_t> left;
+  std::vector<int32_t> right;
+  std::string line;
+
+  while (std::getline(file, line)) {
+    std::istringstream lineStream(line);
+    if (int numRight, numLeft; lineStream >> numLeft >> numRight) {
+      left.push_back(numLeft);
+      right.push_back(numRight);
+    }
+  }
+
+  const std::unordered_multiset multiset(right.begin(), right.end());
+
+  int result = 0;
+  for (int currentNumber: left) {
+    result += currentNumber * multiset.count(currentNumber);
+  }
+
+  return result;
+}
---
+++ 2024/day01/day01.h
@@ -0,0 +1,12 @@
+#ifndef DAY01_H
+#define DAY01_H
+#include <util.h>
+
+class Day01 final : public util::Day {
+public:
+    int64_t part1(std::ifstream &file) override;
+
+    int64_t part2(std::ifstream &file) override;
+};
+
+#endif //DAY01_H
