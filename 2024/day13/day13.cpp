4a67788fe21140e4767ef4355598fee8a9158c29
Gijsdeman <github@deman.dev>
2024-12-16 17:51:41 +0100

    feat(2024-13): add day 13 solution

---
+++ 2024/day13/day13.cpp
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+#include "day13.h"
+
+long determinant(const std::pair<long, long> &left,
+                 const std::pair<long, long> &right) {
+  return left.first * right.second - left.second * right.first;
+}
+
+long cramer(const std::pair<long, long> &target, const std::pair<long, long> &A,
+            const std::pair<long, long> &B) {
+  const long detA = determinant(A, B);
+  const long detA1 = determinant(target, B);
+  const long detA2 = determinant(A, target);
+  if (detA1 % detA == 0 && detA2 % detA == 0) {
+    return 3 * detA1 / detA + detA2 / detA;
+  }
+  return 0;
+}
+
+void Day13::processPush(long &result, std::pair<long, long> &A,
+                        std::pair<long, long> &B,
+                        std::pair<long, long> &target) {
+  if (A.first != -1 && B.first != -1 && target.first != -1) {
+    result += cramer(target, A, B);
+    A = B = target = {-1, -1};
+  }
+}
+
+int64_t Day13::calculateTokens(std::ifstream &file, long addition) {
+  std::string line;
+  int64_t result = 0;
+  const std::regex pattern("X[+=](\\d+), Y[+=](\\d+)");
+  std::pair<long, long> A = {-1, -1}, B = {-1, -1}, target = {-1, -1};
+
+  while (std::getline(file, line)) {
+    const auto matches_begin =
+        std::sregex_iterator(line.begin(), line.end(), pattern);
+    auto matches_end = std::sregex_iterator();
+
+    for (auto it = matches_begin; it != matches_end; ++it) {
+      const auto &match = *it;
+      const auto first = std::stoi(match.str(1));
+      const auto second = std::stoi(match.str(2));
+
+      if (A.first == -1)
+        A = {first, second};
+      else if (B.first == -1)
+        B = {first, second};
+      else
+        target = {first + addition, second + addition};
+    }
+    processPush(result, A, B, target);
+  }
+  return result;
+}
+
+int64_t Day13::part1(std::ifstream &file) { return calculateTokens(file, 0); }
+
+int64_t Day13::part2(std::ifstream &file) {
+  return calculateTokens(file, 10000000000000);
+}
---
+++ 2024/day13/day13.h
@@ -0,0 +1,24 @@
+#ifndef DAY13_H
+#define DAY13_H
+#include <core.h>
+#include <set>
+
+class Day13 final : public core::Day {
+public:
+  int64_t part1(std::ifstream &file) override;
+
+  int64_t part2(std::ifstream &file) override;
+
+private:
+  static int64_t calculateTokens(std::ifstream &file, long addition);
+
+  static void processPush(long &result, std::pair<long, long> &A,
+                          std::pair<long, long> &B,
+                          std::pair<long, long> &target);
+};
+
+static const core::DayRegistrar registerDay("13", []() -> core::Day * {
+  return new Day13();
+});
+
+#endif // DAY13_H
